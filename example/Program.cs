596f237eb21a0a3799f1170843c43d0bb7309269
Roman0437 <rn1002368@otc.edu>
2025-09-18 10:00:14 -0500

    Add files via upload

---
+++ example/Program.cs
@@ -0,0 +1,64 @@
+﻿//using System.Diagnostics;
+//using System.Runtime.CompilerServices;
+
+//Console.WriteLine("give Numer to see if odd or even");
+//int x = Convert.ToInt32(Console.ReadLine());
+//int y = x % 2;
+//if
+
+
+
+Console.WriteLine("what day off the week is it? (type the number)");
+Console.WriteLine("Monday (1");
+Console.WriteLine("Tuesday (2");
+Console.WriteLine("Wednesday (3");
+Console.WriteLine("Thursday (4");
+Console.WriteLine("Friday (5");
+Console.WriteLine("Sunday (6");
+Console.WriteLine("Saturday (7");
+Console.WriteLine();
+
+Console.Write(">> ");
+
+string x = Console.ReadLine()+"";
+Console.WriteLine();
+
+
+switch (x)
+{
+    case "7":
+        x = $"It's the weekend!";
+
+        break;
+    case "1":
+        x = $"Aack! I hate Mondays!";
+
+        break;
+    case "2":
+        x = $"It's Tuesday!";
+
+        break;
+    case "3":
+        x = $"Hump Daaaay!";
+
+        break;
+    case "4":
+        x = $"It's Thursday!";
+
+        break;
+    case "5":
+        x = $"TGIF!";
+
+        break;
+    case "6":
+        x = $"It's the weekend!";
+
+        break;
+    default:
+        x = $"Please check Math!";
+        break;
+
+}
+Console.WriteLine();
+
+Console.WriteLine(x);
